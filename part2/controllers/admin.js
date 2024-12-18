83239d10af2cda48343663a7efe584fe2cf09d6d
hossam.mustafa <hossam.mustafa@uop.edu.jo>
2024-12-18 09:15:17 +0300

    CRUD

--- part2/controllers/admin.js
+++ part2/controllers/admin.js
@@ -10,10 +10,37 @@ exports.postAddProduct= (req,res,next)=>{
         description:req.body.description
     });
     new_product.save();
-    res.send("Created");
+    res.redirect("/admin/products");
 };
-exports.getEditProduct= (req,res,next)=> {};
-exports.postEditProduct= (req,res,next)=>{};
-exports.postDeleteProduct= (req,res,next)=>{};
+exports.getEditProduct= (req,res,next)=> {
+    const product_id = req.params.ProductId;
+    Product.findById(product_id).then((product)=>{
+        res.render("admin/edit-product",{title:"Edit Product",prod:product});
+    });
 
-exports.getProducts = (req,res,next)=> {};
+};
+exports.postEditProduct= (req,res,next)=>{
+    const product_id = req.body.productId;
+    Product.findById(product_id).then((product)=>{
+        product.name = req.body.name;
+        product.description= req.body.description;
+        product.price = req.body.price;
+        product.save();
+        res.redirect("/admin/products");
+    });
+
+};
+exports.getDeleteProduct= (req,res,next)=>{
+   const product_id = req.params.ProductId;
+   Product.findByIdAndDelete(product_id).then((p)=>{
+        res.redirect("/admin/products");
+   })
+
+};
+
+exports.getProducts = (req,res,next)=> {
+    Product.find().then((allproducts)=>{
+        res.render("admin/products",{title:"All Products",products:allproducts});
+    })
+
+};
